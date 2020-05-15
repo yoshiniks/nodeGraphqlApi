d0b62383a31ac7312e1a33a861d87f0a5efc1cd7
Andre <programdrex@gmail.com>
2020-05-15 17:53:07 -0300

    finishing graphql api

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
 const { uuid } = require('uuidv4');
+const graphqlHttp = require('express-graphql');
 
-const key = require('./key');
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolvers');
+const auth = require('./middleware/is-auth');
+const { clearImage } = require('./util/file');
+const key = require('./util/key');
 
 const app = express();
 
@@ -14,8 +19,8 @@ const fileStorage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'images');
     },
-    filename: function(req, file, cb) {
-        let extension = file.originalname.split('.').pop();         
+    filename: (req, file, cb) => {
+        let extension = file.originalname.split('.').pop();           
         cb(null, uuid()+'.'+extension);
     }
 });
@@ -45,8 +50,43 @@ app.use((req, res, next) => {
       'OPTIONS, GET, POST, PUT, PATCH, DELETE'
     );
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if(req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
     next();
   });
+
+app.use(auth);
+
+app.put('/post-image', (req, res, next) => {
+    if (!req.isAuth) {
+      throw new Error('Not authenticated!');
+    }
+    if (!req.file) {
+      return res.status(200).json({ message: 'No file provided!' });
+    }
+    if (req.body.oldPath) {
+      clearImage(req.body.oldPath);
+    }
+    return res
+      .status(201)
+      .json({ message: 'File stored.', filePath: req.file.path.replace("\\", "/") });
+  });
+
+app.use('/graphql', graphqlHttp({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+    graphiql: true,
+    formatError(err) {
+        if(!err.originalError) {
+            return err;
+        }
+        const data = err.originalError.data;
+        const message = err.message || 'An error ocurred!';
+        const code = err.originalError.code || 500;
+        return { message: message, status: code, data: data}
+    }
+}));
 
 //error middleware
 app.use((error, req, res, next) => {
@@ -64,5 +104,4 @@ mongoose.connect(key.MONGODB_URI)
         
     })
     .catch( err => console.log(err));
-
  
--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -3,9 +3,8 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     const authHeader = req.get('Authorization');
     if(!authHeader) {
-        const error = new Error('Not autheticaded!');
-        error.statusCode = 401;
-        throw error;
+        req.isAuth = false;
+        return next();
     }
     
     const token = authHeader.split(' ')[1];
@@ -14,15 +13,15 @@ module.exports = (req, res, next) => {
     try {
         decodedToken = jwt.verify(token, 'somesupersecretsecret');
     } catch(err) {
-        err.statusCode = 500;
-        throw err;
+        req.isAuth = false;
+        return next();
     }
 
     if(!decodedToken) {
-        const error = new Error('Not autheticaded!');
-        error.statusCode = 401;
-        throw error;
+        req.isAuth = false;
+        return next();
     }
     req.userId = decodedToken.userId;
+    req.isAuth = true;
     next();
 };
